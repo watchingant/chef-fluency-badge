f1268409a920b2517a545b233a22a94289ece1a5
Ant Banks <anthony.d.watson@gmail.com>
2017-11-12 03:09:00 +0000

    initial commit of chef repo

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "awatson"
+client_key               "#{current_dir}/awatson.pem"
+chef_server_url          "https://watchingant2.mylabserver.com/organizations/thebizness"
+cookbook_path            ["#{current_dir}/../cookbooks"]
